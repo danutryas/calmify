cce1e0d14d563ab3c8c2dead6ce2c1a0b83b4fab
DuskCelox <88703181+brianashari18@users.noreply.github.com>
2023-05-22 06:35:14 +0700

    Add new assets - images

---
+++ src/img/index.js
@@ -0,0 +1,23 @@
+import HeroImg from "../img/landing-page/hero.png";
+import IconPengalaman from "../img/landing-page/icon/iconPengalaman.png";
+import IconPenghargaan from "../img/landing-page/icon/iconPenghargaan.png";
+import SupportImg from "../img/landing-page/support.png";
+import EarthIcon from "../img/landing-page/icon/earthIcon.png";
+import YellowQuote from "../img/landing-page/yellowQuote.png";
+import BlueQuote from "../img/landing-page/blueQuote.png";
+import PinkQuote from "../img/landing-page/pinkQuote.png";
+import ArticleImg from "../img/landing-page/article1.png";
+import Rainbow from "../img/landing-page/rainbow.png";
+
+export {
+    HeroImg,
+    IconPengalaman,
+    IconPenghargaan,
+    SupportImg,
+    EarthIcon,
+    YellowQuote,
+    BlueQuote,
+    PinkQuote,
+    ArticleImg,
+    Rainbow
+};
