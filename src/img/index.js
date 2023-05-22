0edb8544ba0c1569fcf84f560f8eb4d1da6abc74
DuskCelox <88703181+brianashari18@users.noreply.github.com>
2023-05-23 05:03:59 +0700

    Update index.js

--- src/img/index.js
+++ src/img/index.js
@@ -1,6 +1,10 @@
 import HeroImg from "../img/landing-page/hero.png";
 import IconPengalaman from "../img/landing-page/icon/iconPengalaman.png";
 import IconPenghargaan from "../img/landing-page/icon/iconPenghargaan.png";
+import MindfulnessHubIcon from "../img/landing-page/icon/mindfulnessHelp.png"
+import SupportNetIcon from "../img/landing-page/icon/supportNet.png"
+import MindfulTipsIcon from "../img/landing-page/icon/mindfulTips.png"
+import ChatMind from "../img/landing-page/icon/chatMind.png"
 import SupportImg from "../img/landing-page/support.png";
 import EarthIcon from "../img/landing-page/icon/earthIcon.png";
 import YellowQuote from "../img/landing-page/yellowQuote.png";
@@ -8,16 +12,26 @@ import BlueQuote from "../img/landing-page/blueQuote.png";
 import PinkQuote from "../img/landing-page/pinkQuote.png";
 import ArticleImg from "../img/landing-page/article1.png";
 import Rainbow from "../img/landing-page/rainbow.png";
+import AktivitasIcon from "../img/dashboard/aktivitasIcon.png";
+import YearIcon from "../img/dashboard/yearIcon.png";
+import RatingIcon from "../img/dashboard/ratingIcon.png"
 
 export {
     HeroImg,
     IconPengalaman,
     IconPenghargaan,
+    MindfulnessHubIcon,
+    SupportNetIcon,
+    MindfulTipsIcon,
+    ChatMind,
     SupportImg,
     EarthIcon,
     YellowQuote,
     BlueQuote,
     PinkQuote,
     ArticleImg,
-    Rainbow
+    Rainbow,
+    AktivitasIcon,
+    YearIcon,
+    RatingIcon
 };
