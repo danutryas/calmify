c8f34d82b28423c881febb66054b5125cf604ea8
Danu Tryas <63801773+danutryas@users.noreply.github.com>
2023-05-25 05:58:28 +0700

    [ADD] community page and post element

--- src/pages/community.jsx
+++ src/pages/community.jsx
@@ -1,24 +1,13 @@
-import { useEffect, useRef } from "react";
-import { ImageIcon, MessageIcon, SendIcon } from "../Components/Icons";
 import Footer from "../Components/footer";
 import Header from "../Components/header";
+import Post from "../Components/post";
+import CreatePost from "../Components/createPost";
 
 const CommunityPage = () => {
-  const textAreaEl = useRef(null);
-
-  const onSubmitPost = () => {};
-
-  function autoResize(e) {
-    e.target.style.height = "auto";
-    e.target.style.height = e.target.scrollHeight + "px";
-  }
-  const onClickArea = () => {
-    textAreaEl.current.focus();
-  };
   return (
-    <div className="">
+    <>
       <Header />
-      <section className="mb-48">
+      <section className="mb-48 -mt-14">
         <div className="container bg-white text-lg flex flex-col gap-16 items-center mx-auto">
           <h4 className="text-3xl font-bold  text-[#D23C8F]">Support Net</h4>
           <h1 className="text-[#272D3D] text-7xl font-title">Community</h1>
@@ -40,44 +29,13 @@ const CommunityPage = () => {
               dengan komunitas?
             </p>
           </div>
-          <div className="flex flex-col gap-20">
-            <div className="flex flex-col gap-7 rounded-2xl bg-white post-box-shadow p-16">
-              <h5 className="font-title text-5xl">Create Post</h5>
-              <div
-                className="py-10 px-14 bg-[#F4F5F7] rounded-3xl flex gap-10 cursor-text"
-                onClick={onClickArea}
-              >
-                <MessageIcon />
-                <textarea
-                  placeholder="Bagaimana kabarmu, Nata?"
-                  className="border-none outline-none bg-transparent w-full resize-none overflow-hidden text-xl "
-                  ref={textAreaEl}
-                  onChange={(e) => autoResize(e)}
-                />
-              </div>
-              <div className="flex justify-between">
-                <label
-                  className="flex items-center gap-4 cursor-pointer"
-                  htmlFor="add-image"
-                >
-                  <ImageIcon />
-                  <p className="text-[#000000B2] text-2xl">Image</p>
-                </label>
-                <input type="file" id="add-image" className="hidden" />
-                <button
-                  className="bg-[#2E2BA6] flex text-white gap-4 py-4 px-10 rounded-lg box-shadow-blue font-bold text-xl items-center"
-                  onClick={onSubmitPost}
-                >
-                  <SendIcon />
-                  <span>Submit</span>
-                </button>
-              </div>
-            </div>
-          </div>
+          <CreatePost />
+          <Post />
+          <Post />
         </div>
       </section>
       <Footer />
-    </div>
+    </>
   );
 };
 
---
+++ src/pages/detailPost.jsx
@@ -0,0 +1,11 @@
+import Post from "../Components/post";
+
+const DetailPostPage = () => {
+  return (
+    <div className="">
+      <Post />
+    </div>
+  );
+};
+
+export default DetailPostPage;
