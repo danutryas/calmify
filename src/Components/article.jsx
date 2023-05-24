1136e8f3d4e84acdb025f13b256003ee4fda9304
Brian Anashari <brianashari18@gmail.com>
2023-05-24 18:14:56 +0700

    update article features

--- src/Components/article.jsx
+++ src/Components/article.jsx
@@ -7,10 +7,11 @@ function Article() {
             <Header />
 
             <section id="title">
-                <div className="container bg-white text-lg mx-auto p-10 flex flex-col gap-10 items-center mb-10">
-                    <h4 className="text-3xl font-bold mb-10">Mindfulness Hub</h4>
-                    <h2 className="text-6xl font-title text-mainDark">Bersama Dalam Dukungan, Menguatkan Jiwa</h2>
-                    <h5 className="text-xl text-mainDark">Temukan ketenangan dan keseimbangan dalam hidup Anda. Dapatkan akses mudah ke beragam sumber daya dan latihan mindfulness yang membantu Anda meningkatkan kesadaran diri, mengurangi stres, dan meningkatkan kesejahteraan secara keseluruhan. Nikmati meditasi terpandu, latihan pernapasan, dan teknik mindfulness lainnya di Mindfulness Hub.</h5>
+                <div className="container text-mainText bg-white text-lg mx-auto text-center p-10 flex flex-col gap-10 items-center mb-10">
+                    <h4 class="text-xl font-bold">Mindfulness Hub</h4>
+                    <h2 class="text-6xl font-title text-mainDark">Article</h2>
+                    <h2 className="text-6xl font-title text-[#000] px-56">Bersama Dalam Dukungan, Menguatkan Jiwa</h2>
+                    <h5 className="text-xl text-mainDark px-56">Temukan ketenangan dan keseimbangan dalam hidup Anda. Dapatkan akses mudah ke beragam sumber daya dan latihan mindfulness yang membantu Anda meningkatkan kesadaran diri, mengurangi stres, dan meningkatkan kesejahteraan secara keseluruhan. Nikmati meditasi terpandu, latihan pernapasan, dan teknik mindfulness lainnya di Mindfulness Hub.</h5>
                 </div>
             </section>
 
--- src/Components/footer.jsx
+++ src/Components/footer.jsx
@@ -31,4 +31,3 @@ function Footer() {
 }
 
 export default Footer;
-export default Footer;
